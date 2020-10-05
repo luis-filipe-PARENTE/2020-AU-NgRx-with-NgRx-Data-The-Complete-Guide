57bae11ff18d1d777786cfb1666a5875d8511025
Filipe Parente <lfmparente@gmail.com>
2020-10-05 23:37:13 +0200

    Configuring an NgRx Feature Module using NgRx Schematics

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,13 @@
+import { ActionReducerMap } from '@ngrx/store';
+
+export const featurekey = 'auth';
+
+
+export interface State {
+
+}
+
+export const reducers: ActionReducerMap<State> = {
+
+};
+
