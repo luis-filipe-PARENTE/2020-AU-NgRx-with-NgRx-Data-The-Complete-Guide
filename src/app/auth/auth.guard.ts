6ad118bc45eef16874c47f81c3c94e9b95e94c8e
Luis Filipe PARENTE <luisfilipe.parente.ext@aviva.com>
2020-10-18 23:16:47 +0200

    clean code

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,31 +1,20 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import {
+    ActivatedRouteSnapshot,
+    CanActivate,
+    CanLoad,
+    Route,
+    Router,
+    RouterStateSnapshot,
+    UrlSegment,
+} from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { AppState } from 'app/state/reducers';
-import { noop, Observable, of } from 'rxjs';
-import { map, take, tap } from 'rxjs/operators';
-import { AuthActions } from './action-types';
+import { noop, Observable } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
+
 import { isLoggedIn } from './auth.selectors';
-import { User } from './model/user.model';
 
-
-const makeDispatchLoggedUser = (store: Store<AppState>, ) => {
-        const user: User = JSON.parse(localStorage.getItem('user')) as User;
-        store.dispatch(AuthActions.login({user}))
-        Promise.resolve(
-            store
-            .pipe(
-                select(isLoggedIn),
-                tap(loggedIn => console.log(`[ canLoad ] value of loggedIn ==> ${loggedIn}`)),
-                tap(loggedIn => {
-                    if(!loggedIn) {
-                        this.router.navigateByUrl('/login'); 
-                    }
-                }),
-                take(1)
-            ));
-  }
-  
 
 @Injectable({
     providedIn: 'root'
