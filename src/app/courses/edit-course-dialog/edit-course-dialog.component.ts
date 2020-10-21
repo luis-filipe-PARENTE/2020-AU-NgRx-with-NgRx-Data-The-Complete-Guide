5eb07a8d5b64f3433dd3b564ef1ccc1088cc52a0
Luis Filipe PARENTE <luisfilipe.parente.ext@aviva.com>
2020-10-21 15:40:36 +0200

    Optimistically Editing Entity Data - The Edit Course Dialog

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -4,6 +4,10 @@ import {Course} from '../model/course';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {CoursesHttpService} from '../services/courses-http.service';
+import { AppState } from 'app/state/reducers';
+import { Store } from '@ngrx/store';
+import { Update } from '@ngrx/entity';
+import { courseUpdated } from '../state/course.actions';
 
 @Component({
   selector: 'course-dialog',
@@ -26,7 +30,8 @@ export class EditCourseDialogComponent {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<EditCourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data,
-    private coursesService: CoursesHttpService) {
+    private store: Store<AppState>
+  ) {
 
     this.dialogTitle = data.dialogTitle;
     this.course = data.course;
@@ -57,19 +62,18 @@ export class EditCourseDialogComponent {
   }
 
   onSave() {
-
     const course: Course = {
       ...this.course,
       ...this.form.value
     };
 
-    this.coursesService.saveCourse(course.id, course)
-      .subscribe(
-        () => this.dialogRef.close()
-      )
+    const update: Update<Course> = {
+      id: course.id,
+      changes: course
+    }
 
+    this.store.dispatch(courseUpdated({update}))
+    this.onClose();
 
   }
-
-
 }
