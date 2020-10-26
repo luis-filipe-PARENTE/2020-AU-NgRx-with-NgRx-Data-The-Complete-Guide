a54c5aedddfde51771f338cfd8803d59cb3bd7cd
Luis Filipe PARENTE <luisfilipe.parente.ext@aviva.com>
2020-10-26 23:17:39 +0100

    mplementing the Course Component Using NgRx Data

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,10 +1,12 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {Observable} from 'rxjs';
-import {Lesson} from '../model/lesson';
-import {concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { concatMap, map, tap } from 'rxjs/operators';
+
+import { Course } from '../model/course';
+import { Lesson } from '../model/lesson';
+import { CourseEntityService } from '../services/course-entity.service';
+import { LessonEntityService } from '../services/lesson-entity.service';
 
 
 @Component({
@@ -23,21 +25,27 @@ export class CourseComponent implements OnInit {
   nextPage = 0;
 
   constructor(
-    private coursesService: CoursesHttpService,
+    private courseEntityService: CourseEntityService,
+    private lessonEntityService: LessonEntityService,
     private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
 
-    const courseUrl = this.route.snapshot.paramMap.get("courseUrl");
+    const { params: {courseUrl} } = this.route.snapshot.paramMap as any;
+    debugger;
 
-    this.course$ = this.coursesService.findCourseByUrl(courseUrl);
+    this.course$ = this.courseEntityService.entities$.pipe(
+      map(courses => courses.find(course => course.url === courseUrl))
+    )
 
-    this.lessons$ = this.course$.pipe(
-      concatMap(course => this.coursesService.findLessons(course.id)),
-      tap(console.log)
-    );
+    // this.lessons$ = this.course$.pipe(
+    //   concatMap(course => this.lessonEntityService.findLessons(course.id)),
+    //   tap(console.log)
+    // );
+
+    this.lessons$ = of([]);
 
   }
 
